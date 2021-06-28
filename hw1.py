b343c8ac1aa60b319717ea527fe271fb504fa7ea
spg1101 <sharping@SharPingui-MacBookPro.local>
2021-06-28 19:21:17 +0900

    m

---
+++ hw1.py
@@ -0,0 +1,16 @@
+﻿str1 = "yesterday all my troubles seemed so far away"
+print(str1)
+
+numOfMoum = 0
+numOfJaum = 0
+
+for i in range(0, len(str1), 1):
+
+    if str1[i] != ' ':
+        if str1[i] in 'aeiou':
+            numOfMoum += 1
+        else:
+            numOfJaum += 1
+
+print('모음의 개수 :', numOfMoum)
+print('자음의 개수 :', numOfJaum)
---
+++ hw2.py
@@ -0,0 +1,10 @@
+﻿numOfLine = int( input('출력 줄수 입력 : ') )
+
+for a in range(0, numOfLine, 1):
+    for x in range(numOfLine-1-a, 0, -1):
+        print(' ', end='')
+
+    for x in range(0, 2*a+1, 1):
+        print('*', end='')
+
+    print('')
